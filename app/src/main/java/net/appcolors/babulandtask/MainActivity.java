766bcc32369917e3951f8ef9c7ca5c919398a5b7
Sakibhs <mdsakibhossainbd@gmail.com>
2021-11-11 21:22:40 +0600

    1st commit

---
+++ app/src/main/java/net/appcolors/babulandtask/MainActivity.java
@@ -0,0 +1,15 @@
+package net.appcolors.babulandtask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Hello Babuland
+    }
+}
